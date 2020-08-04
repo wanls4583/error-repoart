c5aa9b5a5bff0cb323eecc95f3af7958c1f73e3f
lisong <409051987@qq.com>
2020-08-04 11:42:31 +0800

    [master]

--- modjs/dao.js
+++ modjs/dao.js
@@ -18,9 +18,62 @@ module.exports = class DAO {
     }
     //添加用户
     save(message, userAgent) {
-        var addSql = 'INSERT IGNORE INTO error_message(message, user_agent) VALUES(?, ?)';
-        var addSqlParams = [message, userAgent];
+        var messageObj = {};
+        var browser = '';
+        try {
+            browser = _getBrowser(userAgent);
+            messageObj = JSON.parse(message);
+        } catch (e) {}
+        var addSql = 'INSERT IGNORE INTO error_message(message, user_agent, browser, error_msg, line, column, src) VALUES(?, ?, ?, ?, ?, ?, ?)';
+        var addSqlParams = [message, userAgent, browser, messageObj[0], messageObj[2], messageObj[3], messageObj[1]];
         return this._select(addSql, addSqlParams);
+
+        function _getBrowser(userAgent) {
+            var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
+            var isIE = userAgent.indexOf("compatible") > -1 &&
+                userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器
+            var isEdge = userAgent.indexOf("Edge") > -1; //判断是否IE的Edge浏览器
+            var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器
+            var isSafari = userAgent.indexOf("Safari") > -1 &&
+                userAgent.indexOf("Chrome") == -1; //判断是否Safari浏览器
+            var isChrome = userAgent.indexOf("Chrome") > -1 &&
+                userAgent.indexOf("Safari") > -1; //判断Chrome浏览器
+
+            if (isIE) {
+                var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+                reIE.test(userAgent);
+                var fIEVersion = parseFloat(RegExp["$1"]);
+                if (fIEVersion == 7) {
+                    return "IE7";
+                } else if (fIEVersion == 8) {
+                    return "IE8";
+                } else if (fIEVersion == 9) {
+                    return "IE9";
+                } else if (fIEVersion == 10) {
+                    return "IE10";
+                } else if (fIEVersion == 11) {
+                    return "IE11";
+                } else {
+                    return "IE";
+                } //IE版本过低
+                return "IE";
+            }
+            if (isOpera) {
+                return "Opera";
+            }
+            if (isEdge) {
+                return "Edge";
+            }
+            if (isFF) {
+                return "FF";
+            }
+            if (isSafari) {
+                return "Safari";
+            }
+            if (isChrome) {
+                return "Chrome";
+            }
+        }
     }
     //查询用户
     get(page, size) {
